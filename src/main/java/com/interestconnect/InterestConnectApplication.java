a8c7389fc8a825a9eb07126732e51e29bfac8055
abhina-v <135498865+abhina-v@users.noreply.github.com>
2026-09-20 17:27:27 +0530

    Update InterestConnectApplication.java

--- src/main/java/com/interestconnect/InterestConnectApplication.java
+++ src/main/java/com/interestconnect/InterestConnectApplication.java
@@ -3,11 +3,12 @@ package com.interestconnect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+import org.springframework.cache.annotation.EnableCaching;
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class InterestConnectApplication {
 
     public static void main(String[] args) {
